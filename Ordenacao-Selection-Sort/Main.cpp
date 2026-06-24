ff8ff75428cad43464396a2aed8ab0b00253da64
Gixya <giovannafgoncalves10@gmail.com>
2026-06-24 11:26:31 -0300

    Add Selection Sort implementation in Main.cpp
    Implement Selection Sort algorithm to sort an array.

---
+++ Ordenacao-Selection-Sort/Main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+//Ordenção implementando Selection Sort.
+
+int main() {
+
+    int v[] = {49, 38, 58, 87, 34, 93, 26, 13};
+    int tamanho = 8;
+
+    cout << "Vetor antes da ordenacao:\n";
+
+    for (int i = 0; i < tamanho; i++) {
+        cout << v[i] << " ";
+    }
+
+    cout << endl;
+
+    // Selection Sort
+    for (int i = 0; i < tamanho - 1; i++) {
+
+        int menor = i;
+
+        for (int j = i + 1; j < tamanho; j++) {
+            if (v[j] < v[menor]) {
+                menor = j;
+            }
+        }
+
+        int temp = v[i];
+        v[i] = v[menor];
+        v[menor] = temp;
+    }
+
+    cout << "\nVetor apos a ordenacao:\n";
+
+    for (int i = 0; i < tamanho; i++) {
+        cout << v[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
